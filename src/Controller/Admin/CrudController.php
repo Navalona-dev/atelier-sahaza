71a60dd32bb6a5348a012f9edd4bdf7573cfd9f3
navalona <nnavalona@gmail.com>
2024-05-16 18:05:12 +0300

    fix crud

--- src/Controller/Admin/CrudController.php
+++ src/Controller/Admin/CrudController.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CrudController extends AbstractController
 {
@@ -23,37 +25,112 @@ class CrudController extends AbstractController
     }
 
    /**
-     * @Route("/admin/nouveau", name="app_admin_new")
+     * @Route("/data/new", name="app_admin_new", methods={"POST"})
      */
     public function new(Request $request): Response
     {
-        $category = new Category();
+        $menu = $request->get('menu');
+       
+        switch ($menu) {
+            case 'categorie':
+                $category = new Category();
 
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
+                $form = $this->createForm(CategoryType::class, $category);
+                
+                $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $date = new \DateTime();
-            $category->setCreatedAt($date)
-                    ->setIsActive(1);
+                if ($form->isSubmitted() && $form->isValid()) {
+                    
+                    $date = new \DateTime();
+                    $category->setCreatedAt($date)
+                            ->setIsActive(1);
+                    
+                    $this->em->persist($category);
+                    $this->em->flush();
 
-            $this->em->persist($category);
-            $this->em->flush();
+                    if ($request->isXmlHttpRequest()) {
+                        return new JsonResponse(['status' => 'success'], Response::HTTP_OK);
+                    }
 
-            if ($request->isXmlHttpRequest()) {
-                return new JsonResponse(['status' => 'success'], Response::HTTP_OK);
-            }
-
-            $this->addFlash('success', 'Categorie ajouté avec succès');
-            return $this->redirectToRoute('app_admin_liste');
+                    $this->addFlash('success', 'Categorie ajouté avec succès');
+                    return $this->redirectToRoute('app_admin_liste');
+                }
+                break;
+            case 'produit':
+                break;
+            default:
+                # code...
+                break;
         }
-
-        if ($request->isXmlHttpRequest()) {
-            return new JsonResponse(['status' => 'error', 'message' => 'Form is invalid'], Response::HTTP_BAD_REQUEST);
-        }
+        
 
         return $this->render('admin/liste/new.html.twig', [
             'form' => $form->createView(),
+            'menu' => $menu
+        ]);
+    }
+
+    /**
+     * @Route("/data/delete/{id}", name="app_admin_delete", methods={"POST"})
+     */
+    public function delete($id, Request $request, CategoryRepository $categoryRepository): Response
+    {
+        
+        $category = $categoryRepository->find($id);
+
+        if ($category) {
+            $this->em->remove($category);
+            $this->em->flush();
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse(['status' => 'success'], Response::HTTP_OK);
+            }
+        }
+        return new Response("ok");
+    }
+
+    /**
+     * @Route("/data/update/{id}", name="app_admin_update", methods={"POST"})
+     */
+    public function update($id, Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $menu = $request->get('menu');
+
+        switch ($menu) {
+            case 'categorie':
+                $category = $categoryRepository->find($id);
+        
+                $form = $this->createForm(CategoryType::class, $category);
+                
+                $form->handleRequest($request);
+
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $date = new \DateTime();
+                    $category->setCreatedAt($date)
+                            ->setIsActive(1);
+                    
+                    $this->em->persist($category);
+                    $this->em->flush();
+
+                    if ($request->isXmlHttpRequest()) {
+                        return new JsonResponse(['status' => 'success'], Response::HTTP_OK);
+                    }
+
+                    $this->addFlash('success', 'Categorie ajouté avec succès');
+                    return $this->redirectToRoute('app_admin_liste');
+                }
+
+                break;
+            case 'produit':
+                break;
+            default:
+                # code...
+                break;
+        }
+        
+        return $this->render('admin/liste/modal_update.html.twig', [
+            'form' => $form->createView(),
+            'id' => $request->get('id'),
+            'menu' => $menu
         ]);
     }
 
